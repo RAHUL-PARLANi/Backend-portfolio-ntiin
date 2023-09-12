8781a6eab0c96945d9b016b3df4033333c7ea05d
Nitin-09 <nitingangwani982000@gmail.com>
2023-09-12 12:17:58 +0530

    backend ready to deploy

--- functions/api.js
+++ functions/api.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const sendMail = require('../MailOwner.js')
 const cors = require('cors')
+const serverless =require('serverless-http') 
 
 const app = express()
 const port = 5000
@@ -22,3 +23,5 @@ app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
 })
 
+module.exports = app
+module.exports.handler = serverless(app)
