2c9ad2b90a9648b75d892df2b83986e5b26e0dc1
RAHUL-PARLANi <90039036+RAHUL-PARLANi@users.noreply.github.com>
2023-09-18 17:21:30 +0530

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -9,6 +9,15 @@ const port = 5000
 app.use(express.json())
 app.use(cors())
 
+app.get('/.netlify/functions/api/contact/all', (req, res) => {
+    try {
+        res.status(200).send( { "message" : "Working" } );
+    }
+    catch (err) {
+        res.status(500).send('Error');
+    }
+})
+
 app.post('/.netlify/functions/api/contact', (req, res) => {
     const { name, email, message } = req.body;
     try {
@@ -16,7 +25,7 @@ app.post('/.netlify/functions/api/contact', (req, res) => {
         res.status(200).send('Email sent successfully');
     }
     catch (err) {
-        res.status(500).send('Error sending email',errsendMailToUser);
+        res.status(500).send({'Error sending email'});
     }
 })
 app.listen(port,()=>{
